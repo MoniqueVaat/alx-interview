45c3919433fd5b19ddb8383b48827a6560ac37f6
MoniqueVaat <Vaatyoughm@gmail.com>
2024-06-19 08:29:00 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
-"""Prime Game"""
+"""
+Module: Game of choosing Prime numbers
+"""
+
+
+def primeNumbers(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
 
 
 def isWinner(x, nums):
     """
-    Calculate the winner of the game
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
     """
-    def calculate_winner(num, memo):
-        if num < 2:
-            return "Ben"
-
-        if memo[num] is not None:
-            return memo[num]
-
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                if calculate_winner(num // i, memo) == "Ben":
-                    memo[num] = "Maria"
-                    return "Maria"
-
-        memo[num] = "Ben"
-        return "Ben"
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        memo = [None] * (n + 1)
-        winner = calculate_winner(n, memo)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    Maria = Ben = 0
+    for i in range(x):
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
